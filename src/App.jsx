b8b147488283f45dabdc3d9ab96f4c0b70aacc3f
Niva02M <nivv2059@gmail.com>
2024-08-29 15:42:47 +0545

    flip pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
-import React, { useState, useRef } from "react";
+import React from "react";
+import HTMLFlipBook from "react-pageflip";
 import "./index.css";
+
+// Import your images
+import Coverimg from "./assets/cover.png";
 import img from "./assets/1.png";
 import img2 from "./assets/2.png";
 import img3 from "./assets/3.png";
@@ -9,79 +13,46 @@ import img6 from "./assets/6.png";
 import img7 from "./assets/7.png";
 import img8 from "./assets/8.png";
 
-const FlipBook = () => {
-  const images = [img, img2, img3, img4, img5, img6, img7, img8];
+function MyBook() {
+  const images = [Coverimg, img, img2, img3, img4, img5, img6, img7, img8];
 
-  // Calculate the total number of pages (2 images per page)
-  const totalPages = Math.ceil(images.length / 2);
-
-  // State to manage the current page
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const bookRef = useRef(null);
-
-  // Function to go to the next page
-  const goToNextPage = () => {
-    if (currentPage < totalPages) setCurrentPage(currentPage + 1);
-  };
-
-  // Function to go to the previous page
-  const goToPreviousPage = () => {
-    if (currentPage > 1) setCurrentPage(currentPage - 1);
-  };
-
-  // Calculate the current images for the page
-  const leftImageIndex = (currentPage - 1) * 2;
-  const rightImageIndex = leftImageIndex + 1;
-
-  const handleClick = (e) => {
-    if (bookRef.current) {
-      const { clientX } = e;
-      const { left, width } = bookRef.current.getBoundingClientRect();
-      const halfWidth = width / 2;
-      const clickX = clientX - left;
-
-      if (clickX < halfWidth) {
-        // Clicked on the left side, go to previous page
-        goToPreviousPage();
-      } else {
-        // Clicked on the right side, go to next page
-        goToNextPage();
-      }
-    }
+  // Flipbook settings
+  const flipSettings = {
+    width: 300,
+    height: 400,
+    size: "fixed",
+    drawShadow: true, // Ensure shadows are enabled
+    showCover: false, // Ensure cover is shown if needed
+    maxShadowOpacity: 0.5,
+    flippingTime: 1000, // Adjust flipping time if necessary
   };
 
   return (
-    <div className="relative flex items-center justify-center w-full h-screen bg-white">
-      <div
-        className="relative w-[500px] h-[400px]"
-        ref={bookRef}
-        onClick={handleClick}
-      >
-        {/* Page Left */}
-        <div className="absolute w-1/2 h-full top-0 left-0 bg-white p-3 shadow-inner shadow-black/50">
-          {images[leftImageIndex] && (
-            <img
-              src={images[leftImageIndex]}
-              alt={`Image ${leftImageIndex + 1}`}
-              className="w-full h-full object-cover"
-            />
-          )}
+    <div className="flex justify-center items-center bg-red-600 p-4 ">
+      <HTMLFlipBook {...flipSettings} className="bg-red-600 overflow-hidden">
+        <div className="h-[400px]  w-[300px] border-2 border-red-600">
+          <div className="flex flex-col justify-center h-full w-full ">
+            <h1 className="text-2xl text-white">
+              Tap on the cover to view the catalogue
+            </h1>
+            <p className="text-sm text-white">
+              Funfact: click or tap on the page to flip{" "}
+            </p>
+          </div>
         </div>
 
-        {/* Page Right */}
-        <div className="absolute w-1/2 h-full top-0 left-1/2 bg-white p-3 shadow-inner shadow-black/50">
-          {images[rightImageIndex] && (
+        {images.map((image, index) => (
+          <div key={index} className="relative w-full h-full bg-white ">
             <img
-              src={images[rightImageIndex]}
-              alt={`Image ${rightImageIndex + 1}`}
-              className="w-full h-full object-cover"
+              src={image}
+              alt={`Page ${index + 1}`}
+              className="object-cover w-full h-full"
             />
-          )}
-        </div>
-      </div>
+          </div>
+        ))}
+      </HTMLFlipBook>
     </div>
   );
-};
+}
 
-export default FlipBook;
+export default MyBook;
